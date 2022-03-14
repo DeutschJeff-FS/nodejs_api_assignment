f468418b4edffc719b24b1c3b2f2383b9f4963f3
Jeff Deutsch <jadeutsch@student.fullsail.edu>
2022-03-13 22:47:27 -0400

    create and run jest tests for api --all passed

--- services/todoService.js
+++ services/todoService.js
@@ -3,17 +3,13 @@ const axios = require("axios");
 const todoService = () => {
   console.log("Real Todos");
 
-  return axios.get(`${process.env.todosURL}`).then((result) => {
-    return result.data;
-  });
+  return axios.get(`${process.env.todosURL}`).then((result) => result.data);
 };
 
 const todoServiceById = (id) => {
   console.log("Real Todo By Id");
 
-  return axios.get(`${process.env.todosURL}${id}`).then((result) => {
-    return result.data;
-  });
+  return axios.get(`${process.env.todosURL}${id}`).then((result) => result.data);
 };
 
 module.exports = { todoService, todoServiceById };
